659639a76dbed6dcc4f497c908fd8cb42ca4ea85
1DimonD <prodm.work@gmail.com>
2023-10-17 23:48:58 +0300

    Initial commit

---
+++ DBMS.Core/Columns/TimeColumn.cs
@@ -0,0 +1,14 @@
+﻿namespace DBMS.Core.Columns
+{
+    public class TimeColumn : Column
+    {
+        public override string Type { get; set; } = "TIME";
+
+        public TimeColumn(string name) : base(name) { }
+
+        public override bool Validate(string value) => 
+            TimeOnly.TryParse(value, out _) || value == "";
+    }
+
+
+}
---
+++ DBMS.Core/Columns/TimeIntervalColumn.cs
@@ -0,0 +1,19 @@
+﻿namespace DBMS.Core.Columns
+{
+    public class TimeIntervalColumn : Column
+    {
+        public override string Type { get; set; } = "TIME INTERVAL";
+
+        public TimeIntervalColumn(string name) : base(name) { }
+
+        public override bool Validate(string value)
+        {
+            string[] buf = value.Replace(" ", "").Split(',');
+
+            return (buf.Length == 2 && TimeOnly.TryParse(buf[0], out TimeOnly a) &&
+              TimeOnly.TryParse(buf[1], out TimeOnly b) && a<b) || value == "";
+        }
+    }
+
+
+}
